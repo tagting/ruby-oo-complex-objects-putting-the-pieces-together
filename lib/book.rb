f5699cbc33528ab3ae75ba1bf72d12ecb54efaa0
ting <ttzhenng@gmail.com>
2020-12-28 17:12:23 -0500

    commit tree d29ae3f1b8745159b0dce83dac2e20783c61e163

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,32 @@
 
-
+class Book
+    def initialize(title) #passing an arg in on initalize
+        @title = title # instance variable, 
+    end                # are in scope within an instance of a class
+    def title #forgot to use the initalize method; getter method
+        @title
+    end
+    def author=(author) #set a property or an instance var : setter
+        @author = author # like: instance.property = "something"
+    end
+    def author  #to access a property of book; get a getter
+        @author #getters and setters tend to come pairs
+    end
+    def page_count=(num) #setter
+        @page_count = num
+    end
+    
+    def page_count #getter
+        @page_count
+    end
+    def genre=(genre) #setter
+        @genre = genre
+    end
+    
+    def genre #getter
+        @genre
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe
+
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+
+    def initialize(brand) #passing an arg in on initalize
+        @brand = brand # instance variable, are in scope within an instance of a class
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
